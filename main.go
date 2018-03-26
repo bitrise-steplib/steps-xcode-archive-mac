68152193394472ef64d7b4021b120e335f75d90c
godrei <godreikrisztian@gmail.com>
2018-03-26 17:04:36 +0200

    set generic destination

--- main.go
+++ main.go
@@ -372,6 +372,7 @@ func main() {
 	}
 
 	archiveCmd.SetArchivePath(archivePath)
+	archiveCmd.SetCustomOptions([]string{"-destination", "generic/platform=OS X"})
 
 	if configs.OutputTool == "xcpretty" {
 		xcprettyCmd := xcpretty.New(archiveCmd)
